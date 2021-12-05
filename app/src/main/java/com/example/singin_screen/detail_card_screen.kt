093fbc2925cc1cb1735b49a53dc31b8357896e3f
dmitry <gif_goin@mail.ru>
2021-12-05 12:37:02 +0300

    three_first_screens

---
+++ app/src/main/java/com/example/singin_screen/detail_card_screen.kt
@@ -0,0 +1,11 @@
+package com.example.singin_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detail_card_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_card_screen)
+    }
+}
